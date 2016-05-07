7e89f81793f9e943f7c4a3a1d648f59fb2d1373e
Samuel das Chagas <samueldaschagas@gmail.com>
2016-05-07 10:50:38 -0300

    Adicionado boilerplate e alterações em textos do header

---
+++ js/main.js
@@ -0,0 +1,32 @@
+$(function(){
+	// Smooth Scrolling
+	$('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+
+	$('.owl-carousel').owlCarousel({
+	    loop:true,
+	    margin:10,
+	    nav:true,
+	    responsive:{
+	        0:{
+	            items:1
+	        },
+	        600:{
+	            items:3
+	        },
+	        1000:{
+	            items:5
+	        }
+	    }
+	})
+});
